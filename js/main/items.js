5b18f03373556fc009191e2d0d54750ec50a69c8
Auric Vente <mprops@protonmail.com>
2022-11-10 01:54:31 -0600

    Mods

--- js/main/items.js
+++ js/main/items.js
@@ -415,7 +415,7 @@ App.create_item_element = function (mode, item) {
   item.element.append(icon)
 
   if (item.pinned) {
-    App.el(".item_icon", item.element).classList.add("pinned")
+    App.el(".item_icon_container", item.element).classList.add("pinned")
   }
 
   let text = App.create("div", "item_text")
